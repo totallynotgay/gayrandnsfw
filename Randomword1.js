6772858b41a6bd54775a457d6aeecdfa4f471038
totallynotgay <totallynotgay@yopmail.com>
2016-06-06 13:14:44 +0000

    Add files via upload

---
+++ Randomword1.js
@@ -0,0 +1,22 @@
+var words=new Array()
+
+words[0]='toasters'
+words[1]='Nicki Minaj'
+words[2]='sharks'
+words[3]='dolphins'
+words[4]='doors'
+words[5]='George Clooney'
+words[6]='books'
+words[7]='goats'
+words[8]='obama'
+words[9]='magical hair changing person'
+words[10]='Adrien Brody'
+words[11]='chairs'
+words[12]='tables'
+words[13]='phones'
+words[14]='gimps'
+words[15]='batman'
+words[16]='pigs'
+words[17]='commies'
+var whichword=Math.floor(Math.random()*(words.length))
+document.write(words[whichword])
